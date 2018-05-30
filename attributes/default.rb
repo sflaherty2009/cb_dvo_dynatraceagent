aea6ace7125b8543c6e3af489ad2033f769ea8cf
dreweasland <drew_easland@trekbikes.com>
2018-05-30 04:30:28 -0500

    Updates to get prodarm into dynatrace

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,13 +3,13 @@ default['dvo_user']['dynatrace']['windows_installer_archive'] = 'Dynatrace-OneAg
 default['dvo_user']['dynatrace']['infra_only_linux'] = 'INFRA_ONLY=0'
 default['dvo_user']['dynatrace']['infra_only_windows'] = 'INFRA_ONLY="0"'
 
-if node['dvo_user']['ALM_environment'] == 'prod'
+if node['dvo_user']['ALM_environment'].include?('prod')
   default['dvo_user']['dynatrace']['linux_installer'] = 'https://exf44419.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?Api-Token=qotvRmYJRNecrCzrSve1i&arch=x86&flavor=default'
   default['dvo_user']['dynatrace']['windows_installer'] = 'https://exf44419.live.dynatrace.com/api/v1/deployment/installer/agent/windows/default-unattended/latest?Api-Token=qotvRmYJRNecrCzrSve1i&arch=x86&flavor=default'
   default['dvo_user']['dynatrace']['server'] = 'https://exf44419.live.dynatrace.com:443'
   default['dvo_user']['dynatrace']['tenant'] = 'exf44419'
   default['dvo_user']['dynatrace']['tenant_token'] = 'T8GUIYFCSfUNptZf'
-elsif node['dvo_user']['ALM_environment'] == 'testarm'
+elsif node['dvo_user']['ALM_environment'].include?('test')
   default['dvo_user']['dynatrace']['linux_installer'] = 'https://iyo97691.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?Api-Token=pF9nfoHGTAe7_4blu5jIW&arch=x86&flavor=default'
   default['dvo_user']['dynatrace']['windows_installer'] = 'https://iyo97691.live.dynatrace.com/api/v1/deployment/installer/agent/windows/default-unattended/latest?Api-Token=pF9nfoHGTAe7_4blu5jIW&arch=x86&flavor=default'
   default['dvo_user']['dynatrace']['server'] = 'https://iyo97691.live.dynatrace.com:443'
